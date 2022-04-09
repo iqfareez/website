0cfbbfd67c774e7f0c9cf2f9404687de9a31180e
iqfareez <iqmal3@outlook.com>
2022-04-09 18:00:07 +0800

    :pencil2: More personalization touch

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,16 +1,12 @@
 ---
-name: Tails Azimuth
+name: Muhammad Fareez Iqmal
 avatar: /static/images/avatar.png
-occupation: Professor of Atmospheric Science
-company: Stanford University
-email: address@yoursite.com
-twitter: https://twitter.com/Twitter
-linkedin: https://www.linkedin.com
-github: https://github.com
+occupation: Engineering Student
+company: International Islamic University Malaysia
+email: m.fareez.iqmal@gmail.com
+twitter: https://twitter.com/iqfareez
+linkedin: https://www.linkedin.com/iqfareez
+github: https://github.com/iqfareez
 ---
 
-Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
-
-He leads the clean energy group which develops 3D air pollution-climate models, writes differential equation solvers, and manufactures titanium plated air ballons. In his free time he bakes raspberry pi.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+Fareez is a Mechatronics Engineering student at IIUM Gombak who is passionate in developing mobile applications, and electronic systems as a side project.
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
-import projectsData from '@/data/projectsData'
+import projectsDataHighlights from '@/data/projects/highlight'
+import projectsDataAndroid from '@/data/projects/android'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 
@@ -7,27 +8,44 @@ export default function Projects() {
   return (
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Projects
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
-          </p>
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Projects
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          Wondering how I spent my free time?
+        </p>
+      </div>
+      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-14">
+        Highlights
+      </h2>
+      <div className="container py-12">
+        <div className="-m-4 flex flex-wrap">
+          {projectsDataHighlights.map((d) => (
+            <Card
+              key={d.title}
+              title={d.title}
+              description={d.description}
+              imgSrc={d.imgSrc}
+              href={d.href}
+            />
+          ))}
         </div>
-        <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
-              <Card
-                key={d.title}
-                title={d.title}
-                description={d.description}
-                imgSrc={d.imgSrc}
-                href={d.href}
-              />
-            ))}
-          </div>
+      </div>
+      <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-14">
+        Android app
+      </h2>
+      <div className="container py-12">
+        <div className="-m-4 flex flex-wrap">
+          {projectsDataAndroid.map((d) => (
+            <Card
+              key={d.title}
+              title={d.title}
+              description={d.description}
+              imgSrc={d.imgSrc}
+              href={d.href}
+            />
+          ))}
         </div>
       </div>
     </>
