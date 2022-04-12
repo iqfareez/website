392a9eb4732d32bebc047335613b31a196303044
iqfareez <iqmal3@outlook.com>
2022-04-12 11:50:40 +0800

    :heavy_plus_sign: Added heroicons/react
    under construction banner, show home link

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -16,9 +16,7 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
+                  <div className="text-2xl font-semibold sm:block">{siteMetadata.headerTitle}</div>
                 ) : (
                   siteMetadata.headerTitle
                 )}
