5102d92c1266a30ff5404881c465f5be0f54ed77
iqfareez <iqmal3@outlook.com>
2022-06-20 16:37:14 +0800

    :pencil2: Add safety parcel project
    Add some info smart trash bin

--- data/projects/uni.js
+++ data/projects/uni.js
@@ -1,8 +1,10 @@
 const projectsDataUniversity = [
   {
     title: 'Smart Trash Bin',
-    description: `A next level trash bin. Modern and feature pack.
-     An Integrated Design Project (IDP) project.`,
+    description: `An Integrated Design Project (IDP) project. 
+    Some features including: Trash level indicator (LCD display,
+    LEDs indicator) with real time update to app and Telegram, notify
+     subscribers when the bin is full, hand sensor, automatic lid etc.`,
     imgSrc: '/static/images/projects/smart-trash-bin.jpg',
     href: 'https://docs.google.com/presentation/d/1xpqHdYDqdd6iYqOgnxsjHbQ5tGCwdeG6IJbDviTlwqM/edit?usp=sharing',
   },
@@ -12,6 +14,13 @@ const projectsDataUniversity = [
         it is time to pick up your clothes from 'ampaian'. A Industrial Automation project.`,
     imgSrc: '/static/images/projects/ia-weather-station.jpg',
     href: 'https://github.com/iqfareez/IA-Project-Weather-Station',
+  },
+  {
+    title: 'Parcel Safety Box',
+    description: `DIY Weather Station that will notify you when 
+        it is time to pick up your clothes from 'ampaian'. A Industrial Automation project.`,
+    imgSrc: '/static/images/projects/gene-parcel.png',
+    href: 'https://youtu.be/KFbdNpS7P3A',
   },
 ]
 
