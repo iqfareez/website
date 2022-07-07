a4bbf87f2940041c2a3efbd5ed36518025ff46fe
iqfareez <iqmal3@outlook.com>
2022-07-07 10:18:19 +0800

    :chart_with_upwards_trend: Added Google Analytics 4
    Hope it works huhu

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com;
   style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
+  img-src * blob: data: *.google-analytics.com *.googletagmanager.com;
   media-src 'none';
-  connect-src *;
+  connect-src * *.google-analytics.com *.analytics.google.com *.googletagmanager.com;
   font-src 'self';
   frame-src giscus.app
 `
