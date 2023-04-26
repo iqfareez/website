1003a6dfa316f1d63cc4a3946fc80fbdca73dd47
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-04-27 00:47:43 +0800

    🚀 feat(package.json): add @vercel/analytics dependency ✨ feat(_app.js): add @vercel/analytics to track page views and events 🔥 chore(_app.js): remove unused Analytics component and import BuiltInAnalytics component instead

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,11 @@ import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
+import BuiltInAnalytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+
+import { Analytics } from '@vercel/analytics/react'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -22,6 +24,7 @@ export default function App({ Component, pageProps }) {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
+      <BuiltInAnalytics />
       <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
