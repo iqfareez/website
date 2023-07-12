f929600f97730dbbecdeeca3835179ce9f368fcd
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-07-12 12:31:11 +0800

    :see_no_evil: Update easter egg script
    add delay

--- public/iqfareez.sh
+++ public/iqfareez.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Just a fun easter egg haha. Try running this script in your terminal!
+# eg: `curl -s https://iqfareez.com/iqfareez.sh | bash`
+
 echo "Hello there, welcome to this special terminal session!"
+sleep 1
 echo "iqfareez is awesome!"
+sleep 1
 echo "Have a great day :)"
