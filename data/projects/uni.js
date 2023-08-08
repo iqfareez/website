1eb1f5670294e2e700355ec349e06daa28492494
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-08-08 13:17:35 +0000

    :sparkles: Add fyp project

--- data/projects/uni.js
+++ data/projects/uni.js
@@ -1,24 +1,33 @@
 const projectsDataUniversity = [
   {
+    title: 'KOEWayfinder - AR Navigation App',
+    description: `A Final Year Project (FYP) to complete my bachelor degree. 
+    The project aims to address the challenges of indoor navigation within faculty building
+    by using Augmented Reality technology. Utlizes SuperPoint & SuperGlue algorithm
+    to build localization pipeline.`,
+    imgSrc: '/static/images/projects/koe-wayfinder.png',
+    href: 'https://github.com/KOE-Wayfind/',
+  },
+  {
     title: 'Smart Trash Bin',
     description: `An Integrated Design Project (IDP) project. 
-    Some features including: Trash level indicator (LCD display,
-    LEDs indicator) with real time update to app and Telegram, notify
-     subscribers when the bin is full, hand sensor, automatic lid etc.`,
+    Some features including: Interactive trash level indicator, notification
+    through Telegram, hand sensor, automatic lid etc. Also comes with a 
+    mobile app to view realtime status of the bin.`,
     imgSrc: '/static/images/projects/smart-trash-bin.jpg',
     href: 'https://docs.google.com/presentation/d/1xpqHdYDqdd6iYqOgnxsjHbQ5tGCwdeG6IJbDviTlwqM/edit?usp=sharing',
   },
   {
     title: 'IoT Weather Station',
-    description: `DIY Weather Station that will notify you when 
-        it is time to pick up your clothes from 'ampaian'. A Industrial Automation project.`,
+    description: `DIY Weather Station that will keep track of the weather and notify you when 
+        it is time to pick up your clothes from cloths line. An Industrial Automation project.`,
     imgSrc: '/static/images/projects/ia-weather-station.jpg',
     href: 'https://github.com/iqfareez/IA-Project-Weather-Station',
   },
   {
     title: 'Parcel Safety Box',
-    description: `DIY Weather Station that will notify you when 
-        it is time to pick up your clothes from 'ampaian'. A Industrial Automation project.`,
+    description: `Protect your parcel from thief in a PIN-protected slot. Receiver
+    can unlock the slot using PIN that has been send to Telegram. An Engineering Ethics project.`,
     imgSrc: '/static/images/projects/gene-parcel.png',
     href: 'https://youtu.be/KFbdNpS7P3A',
   },
